80128b66ad097c5f91cfbb14e612fb88bcaef8d6
wilo01 <wilk9496@gmail.com>
2022-09-26 14:03:51 +0200

    Project Template

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Styles/styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <main>
+        <section className="container">
+          <section className="info">
+          <h1>hello</h1>
+          </section>
+        </section>
+      </main>
+      <footer className="footer">
+          <h2>hello</h2>
+      </footer>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App name="TEST" />);
